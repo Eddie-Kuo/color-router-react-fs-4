d6f69703128a82f60a16a6212036983052800f7f
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-09-27 11:35:28 -0500

    set up header nav links, changes the URL

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,28 @@
+import { useState } from 'react';
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 export default function Header() {
   return (
-    <header>Header</header>
+    <header>
+      <div>
+        <NavLink to="/rgb/30/144/255">Dodger Blue</NavLink>
+      </div>
+      <div>
+        <NavLink to="/rgb/50/205/50">Lime Green</NavLink>
+      </div>
+      <div>
+        <NavLink to="/rgb/153/50/204">Dark Orchid</NavLink>
+      </div>
+
+      <Switch>
+        <Route path="/rgb/30/144/255" component={Blue} ></Route>
+        <Route path="/rgb/50/205/50" component={Green} ></Route>
+        <Route path="/rgb/153/50/204" component={Orchid} ></Route>
+      </Switch>
+
+    </header>
   );
 }
