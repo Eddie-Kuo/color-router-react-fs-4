5f0fbdca04b113cb01550fc1eb135379377a9556
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-09-28 20:06:37 -0500

    added a home route for on load so its not an error screen

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 import { Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
+import { Redirect } from 'react-router-dom';
 
 
 function App() {
@@ -12,9 +13,15 @@ function App() {
       <Header/>
 
       <Switch>
-        <Route exact path={`/rgb/:red/:green/:blue`} component={Home} ></Route>
-        <Route path="*" component={NotFound} ></Route>
-
+        <Route exact path={`/rgb/:red/:green/:blue`} 
+          component={Home} >
+        </Route>
+        <Route path= "/">
+          <Redirect to={`/rgb/100/100/100`} />
+        </Route>
+        <Route path="*" 
+          component={NotFound} >
+        </Route>
       </Switch>
     </div>
   );
