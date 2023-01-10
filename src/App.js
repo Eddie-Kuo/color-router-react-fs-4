e47b73a03159360b22796cfe66eed96087dccb5f
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-01-09 18:52:54 -0600

    refactored react router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import './App.css';
 import Header from './components/Header/Header';
-import { Route } from 'react-router-dom';
-import { Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
-import { Redirect } from 'react-router-dom';
-
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
 
-      <Switch>
-        <Route exact path={`/rgb/:red/:green/:blue`} 
-          component={Home} >
-        </Route>
-        <Route path= "/">
-          <Redirect to={`/rgb/100/100/100`} />
-        </Route>
-        <Route path="*" 
-          component={NotFound} >
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path={`/rgb/:red/:green/:blue`} element={<Home />} />
+        <Route path="/" element={<Navigate to={`/rgb/100/100/100`} />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
